6ba5247db466443e59558685c3250850e7442ac7
mazbaul20 <mazbaul20@gmail.com>
2026-05-16 12:55:26 +0600

    Add Item

--- lib/ToDoPage.dart
+++ lib/ToDoPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'Style.dart';
+
 class ToDoPage extends StatefulWidget {
   const ToDoPage({super.key});
 
@@ -9,44 +11,83 @@ class ToDoPage extends StatefulWidget {
 }
 
 class ToDoPageView extends State<ToDoPage> {
+  List ToDoList = [];
+  String item="";
 
-  List ToDoList=[{"2":"2"},{"3":"3"},{"4":"4"},{"5":"5"}];
+  MyInputOnChange(content){
+    setState(() {
+      // item=ToDoList.add({"Item":content});
+      item=content;
+    });
+  }
 
+  AddItem(){
+    setState(() {
+      ToDoList.add({"item":item});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Todo App'),
-      ),
+      appBar: AppBar(title: Text('Todo App')),
       body: Container(
         padding: EdgeInsets.all(10),
         child: Column(
           children: [
             Expanded(
-                flex: 10,
-                child: Row(
-                  children: [
-                    Expanded(child: TextFormField()),
-                    Expanded(child: ElevatedButton(onPressed: (){}, child: Text('Add'))),
-                  ],
-                ),
+              flex: 10,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      onChanged: (value){
+                        MyInputOnChange(value);
+                      },
+                      decoration: AppInputDecoration("Enter Task"),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        AddItem();
+                      },
+                      child: Text('Add'),
+                      style: AppButtonStyle(),
+                    ),
+                  ),
+                ],
+              ),
             ),
+            SizedBox(height: 10),
             Expanded(
-                flex: 90,
-                child: ListView.builder(
-                  itemCount: ToDoList.length,
-                  itemBuilder: (context, index){
-                    return Card(
-                      child: Text("All List view"),
-                    );
-                  },
-                ),
+              flex: 90,
+              child: ListView.builder(
+                itemCount: ToDoList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: SizeBox50(
+                      Row(
+                        children: [
+                          Expanded(flex: 80, child: Text(ToDoList[index]['item'].toString())),
+                          Expanded(
+                            flex: 20,
+                            child: TextButton(
+                              onPressed: (){},
+                              child: Icon(Icons.delete,color: Colors.red,),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
     );
   }
-
 }
