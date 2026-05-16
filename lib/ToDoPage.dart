b67cefa0b1f0995c5d83122e932a45e55a10dc56
mazbaul20 <mazbaul20@gmail.com>
2026-05-16 12:05:19 +0600

    Todo app part-2

--- lib/ToDoPage.dart
+++ lib/ToDoPage.dart
@@ -9,13 +9,43 @@ class ToDoPage extends StatefulWidget {
 }
 
 class ToDoPageView extends State<ToDoPage> {
+
+  List ToDoList=[{"2":"2"},{"3":"3"},{"4":"4"},{"5":"5"}];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Todo App'),
       ),
-      body: Text('Todo App'),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Expanded(
+                flex: 10,
+                child: Row(
+                  children: [
+                    Expanded(child: TextFormField()),
+                    Expanded(child: ElevatedButton(onPressed: (){}, child: Text('Add'))),
+                  ],
+                ),
+            ),
+            Expanded(
+                flex: 90,
+                child: ListView.builder(
+                  itemCount: ToDoList.length,
+                  itemBuilder: (context, index){
+                    return Card(
+                      child: Text("All List view"),
+                    );
+                  },
+                ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
